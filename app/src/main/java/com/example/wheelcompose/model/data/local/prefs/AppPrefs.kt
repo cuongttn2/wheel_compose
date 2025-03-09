5440e97ac9d15332c1c001c37296f53368e26a1b
_v <caocuongttn2@gmail.com>
2025-03-09 18:57:34 +0700

    base

---
+++ app/src/main/java/com/example/wheelcompose/model/data/local/prefs/AppPrefs.kt
@@ -0,0 +1,30 @@
+package com.example.wheelcompose.model.data.local.prefs
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+
+object AppPrefs {
+    private lateinit var prefs: SharedPreferences
+    private val LOCK = Any()
+    private val gson: Gson by lazy { Gson() }
+    fun init(context: Context) {
+        if (!AppPrefs::prefs.isInitialized) {
+            synchronized(LOCK) {
+                prefs = PreferenceHelper.newEncryptPrefs(context, "wheel_compose_prefs")
+            }
+        }
+    }
+
+    fun isInitialized() = AppPrefs::prefs.isInitialized
+
+    var language: String
+        set(value) {
+            prefs[APP_LANGUAGE] = value
+        }
+        get() = prefs[APP_LANGUAGE, "en"] ?: "en"
+
+
+    private const val APP_LANGUAGE = "app_language_code"
+
+}
